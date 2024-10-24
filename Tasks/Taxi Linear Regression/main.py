b229a831417415012aad2d83ce6488819e567713
piatnitsssa <141573423+piatnitsssa@users.noreply.github.com>
2024-10-24 07:20:50 +0300

    Update

--- Tasks/Taxi Linear Regression/main.py
+++ Tasks/Taxi Linear Regression/main.py
@@ -1,12 +1,9 @@
 from train import preprocess_data
 from learn import train_and_evaluate_model
 
-# Пути к файлам
 input_csv_path = '/Users/vitaliipiatnitsa/Desktop/ML/training ML/HomwWork_1/Taxi Linear Reggresion/raw_data.csv'
 output_csv_path = '/Users/vitaliipiatnitsa/Desktop/ML/training ML/HomwWork_1/Taxi Linear Reggresion/markup_data.csv'
 
-# Шаг 1: Разметка данных
 preprocess_data(input_csv_path, output_csv_path)
 
-# Шаг 2: Обучение модели и построение графика
 train_and_evaluate_model(output_csv_path)
