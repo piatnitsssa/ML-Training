6e8da0e85dc5ab1423f903ea207e541bdc10e4c7
piatnitsssa <141573423+piatnitsssa@users.noreply.github.com>
2024-10-24 07:21:25 +0300

    Update

--- Tasks/Taxi Linear Regression/main.py
+++ Tasks/Taxi Linear Regression/main.py
@@ -1,8 +1,8 @@
 from train import preprocess_data
 from learn import train_and_evaluate_model
 
-input_csv_path = '/Users/vitaliipiatnitsa/Desktop/ML/training ML/HomwWork_1/Taxi Linear Reggresion/raw_data.csv'
-output_csv_path = '/Users/vitaliipiatnitsa/Desktop/ML/training ML/HomwWork_1/Taxi Linear Reggresion/markup_data.csv'
+input_csv_path = 'path to raw data'
+output_csv_path = 'path to markup data'
 
 preprocess_data(input_csv_path, output_csv_path)
 
